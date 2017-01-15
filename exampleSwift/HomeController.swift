511813b07c5d234e468b132b344e6ebad61937ea
hasam0703 <hasam0703@gmail.com>
2017-01-15 22:26:07 +0700

    done es 1

---
+++ exampleSwift/HomeController.swift
@@ -0,0 +1,103 @@
+//
+//  HomeController.swift
+//  exampleSwift
+//
+//  Created by Developer on 1/15/17.
+//  Copyright © 2017 Developer. All rights reserved.
+//
+
+import UIKit
+
+// declare class for collectionViewCell
+class WordCell: UICollectionViewCell {
+    // init cell
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 0.868218316, alpha: 1)
+        setupViews()
+    }
+    
+    // declare label to add to cell in collectionView
+    let wordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "fucking you"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // add label to cell in collectionView
+    func setupViews() {
+        backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        addSubview(wordLabel)
+        wordLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        wordLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        wordLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    //
+    let cellId = "cellId"
+    let headerId = "headerId"
+    let footerId = "footerId"
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+        self.title = "collectionView"
+        //
+        collectionView?.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        // register cell for collectionView
+        collectionView?.register(WordCell.self, forCellWithReuseIdentifier: cellId)
+        // register header for collectionView
+        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+        // register footer for collectionView
+        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: footerId)
+    }
+    
+    // cell
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.size.width, height: 50)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionElementKindSectionHeader {
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+            header.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+            return header
+        } else {
+            let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: footerId, for: indexPath)
+            footer.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+            return footer
+        }
+    }
+    
+    // header
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.size.width, height: 50)
+    }
+    
+    // footer
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.size.width, height: 50)
+    }
+    
+}
