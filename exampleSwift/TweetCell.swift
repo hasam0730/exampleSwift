d07abe89e93110914a8a375c2d1a004625579dd1
hasam0703 <hasam0703@gmail.com>
2017-02-12 00:14:55 +0700

    done ep 10

--- exampleSwift/TweetCell.swift
+++ exampleSwift/TweetCell.swift
@@ -33,11 +33,11 @@ class TweetCell: DatasourceCell {
         separatorLineView.isHidden = false
         separatorLineView.backgroundColor = UIColor(r: 200, g: 200, b: 200)
         backgroundColor = .white
-        //
+        // 1
         addSubview(messageTextView)
         addSubview(profileImageView)
-        
-        //
+        addSubview(replyButton)
+        // 2
         profileImageView.anchor(topAnchor,
                                 left: leftAnchor,
                                 bottom: nil,
@@ -48,7 +48,7 @@ class TweetCell: DatasourceCell {
                                 rightConstant: 0,
                                 widthConstant: 50,
                                 heightConstant: 50)
-        //
+        // 3
         messageTextView.anchor(topAnchor,
                                left: profileImageView.rightAnchor,
                                bottom: bottomAnchor,
@@ -59,6 +59,7 @@ class TweetCell: DatasourceCell {
                                rightConstant: 0,
                                widthConstant: 0,
                                heightConstant: 0)
+        setupBottomButtons()
     }
     
     let profileImageView: UIImageView = {
@@ -69,11 +70,74 @@ class TweetCell: DatasourceCell {
         return imageView
     }()
     
+    let replyButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "reply").withRenderingMode(.alwaysOriginal), for: .normal)
+       return button
+    }()
+    
+    let retweetButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "retweet").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let likeButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "like").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let directMessButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "send_message").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+
     let messageTextView: UITextView = {
        let messageTextView = UITextView()
         messageTextView.text = "some sample text"
-        messageTextView.backgroundColor = .green
+        messageTextView.backgroundColor = .clear
         messageTextView.isScrollEnabled = false
         return messageTextView
     }()
+    
+    fileprivate func setupBottomButtons() {
+        // 1
+        let replyButtonContainerView = UIView()
+        // 2
+        let retweetButtonContainerView = UIView()
+        // 3
+        let likeButtonContainerView = UIView()
+        // 4
+        let directMessageButtonContainerView = UIView()
+        // 5
+        let buttonStackView = UIStackView(arrangedSubviews: [replyButtonContainerView, retweetButtonContainerView, likeButtonContainerView, directMessageButtonContainerView])
+        buttonStackView.axis = .horizontal
+        buttonStackView.distribution = .fillEqually
+        // 6
+        addSubview(buttonStackView)
+        buttonStackView.anchor(nil,
+                               left: messageTextView.leftAnchor,
+                               bottom: self.bottomAnchor,
+                               right: self.rightAnchor,
+                               topConstant: 0,
+                               leftConstant: 0,
+                               bottomConstant: 5,
+                               rightConstant: 0,
+                               widthConstant: 0,
+                               heightConstant: 20)
+        // 7
+        addSubview(replyButton)
+        replyButton.anchor(replyButtonContainerView.topAnchor, left: replyButtonContainerView.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+        // 8
+        addSubview(retweetButton)
+        retweetButton.anchor(retweetButtonContainerView.topAnchor, left: retweetButtonContainerView.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+        // 9
+        addSubview(likeButton)
+        likeButton.anchor(likeButtonContainerView.topAnchor, left: likeButtonContainerView.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+        // 10
+        addSubview(directMessButton)
+        directMessButton.anchor(directMessageButtonContainerView.topAnchor, left: directMessageButtonContainerView.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+    }
 }
